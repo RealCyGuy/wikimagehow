9a6e484f08ed823fb459433bdecdd61266d232bd
Cyrus Yip <cyruscmyip1@gmail.com>
2021-01-24 18:14:56 -0800

    revert

--- functions/getRandomImages.js
+++ functions/getRandomImages.js
@@ -1,16 +1,10 @@
 const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
-  // try {
-  //   images.length = 0;
-  // } catch {}
   fetch(
     "https://www.wikihow.com/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=5"
   )
     .then((articles) => articles.json())
     .then((articles) => {
-      try {
-        images.length = 0;
-      } catch {}
       articles["query"]["random"].forEach((article) => {
         fetch(
           `https://www.wikihow.com/api.php?format=json&action=parse&prop=images&pageid=${article["id"]}`
