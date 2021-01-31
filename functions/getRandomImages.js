6b4f4e7e49ea7bcd136c5810787a98f238785649
Cyrus Yip <cyruscmyip1@gmail.com>
2021-01-30 21:38:14 -0800

    function works

--- functions/getRandomImages.js
+++ functions/getRandomImages.js
@@ -1,27 +1,32 @@
 const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
-  fetch(
+  return fetch(
     "https://www.wikihow.com/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=5"
   )
     .then((articles) => articles.json())
     .then((articles) => {
-      articles["query"]["random"].forEach((article) => {
-        fetch(
+      return Promise.all(articles["query"]["random"].map(article => {
+        return fetch(
           `https://www.wikihow.com/api.php?format=json&action=parse&prop=images&pageid=${article["id"]}`
         )
           .then((image) => image.json())
           .then((image) => {
-            images.push([
+            image["parse"]["images"].shift();
+            return [
               image["parse"]["title"],
               image["parse"]["images"][
                 Math.floor(Math.random() * image["parse"]["images"].length)
               ],
-            ]);
-          });
-      });
-    });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(images),
-  };
+            ];
+          })
+      }))
+      .then(images => {
+        console.log(images);
+        return images;
+      })
+    })
+    .then((images) => ({
+      statusCode: 200,
+      body: JSON.stringify(images),
+    }));
 };
