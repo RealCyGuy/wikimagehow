386034278d24aad2b6861b09e7c0f6f081de745b
Cyrus Yip <cyruscmyip1@gmail.com>
2021-02-01 11:39:14 -0800

    startup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-console.log("hi");
-
-function getImage() {
-  fetch(
-    "https://www.wikihow.com/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=1"
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+let image = document.querySelector("img");
+let loader = document.querySelector("svg");
+let queue = [];
+let index = 0
+async function startup() {
+  const response = await fetch("/.netlify/functions/getRandomImages");
+  const images = await response.json();
+  queue = images;
+  image.src = queue[0][1];
+  loader.remove();
+  image.classList.remove("hidden");
 }
+startup();
