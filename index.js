7e0ad309288c1040781ac14bfe14d52ffb0ea197
Cyrus Yip <cyruscmyip1@gmail.com>
2021-02-02 08:34:15 -0800

    title text

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let image = document.querySelector(".image");
 let loader = document.querySelector(".loader");
 let back = document.querySelector(".back");
 let next = document.querySelector(".next");
+let title = document.querySelector(".title");
 let queue = [];
-let index = 0
+let index = 0;
 async function startup() {
   const response = await fetch("/.netlify/functions/getRandomImages");
   const images = await response.json();
@@ -12,5 +13,8 @@ async function startup() {
   loader.remove();
   image.classList.remove("hidden");
   next.disabled = false;
+  title.textContent = queue[0][0];
+  title.href = "https://wikihow.com/" + queue[0][0];
+  title.classList.add("underline");
 }
 startup();
